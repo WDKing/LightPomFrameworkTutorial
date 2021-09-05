26c6aaa543e6c9366ed2d8d20c96f74d6a19ab35
William King <will.david.king@gmail.com>
2021-09-05 18:54:35 -0500

    20210905 - Section 23: Drag and Drop

---
+++ UserInteractionsdemo/InteractionsDemo.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+using System;
+using Assert = NUnit.Framework.Assert;
+
+namespace UserInteractionsDemo
+{
+    [TestFixture]
+    public class InteractionsDemo
+    {
+        private IWebDriver driver;
+        private Actions actions;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            actions = new Actions(driver);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(6));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+        [Test]
+        public void Example1()
+        {
+            //var driver = new ChromeDriver();
+            //var actions = new Actions(driver);
+            //var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(6));
+
+            driver.Navigate().GoToUrl("http://jqueryui.com/droppable/");
+            wait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(By.ClassName("demo-frame")));
+
+            IWebElement sourceElement = driver.FindElement(By.Id("draggable"));
+            IWebElement targetElement = driver.FindElement(By.Id("droppable"));
+            actions.DragAndDrop(sourceElement, targetElement).Perform();
+
+            Assert.AreEqual("Dropped!", targetElement.Text);
+        }
+
+        [Test]
+        public void Example2()
+        {
+            driver.Navigate().GoToUrl("http://jqueryui.com/droppable/");
+            wait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(By.ClassName("demo-frame")));
+
+            IWebElement sourceElement = driver.FindElement(By.Id("draggable"));
+            IWebElement targetElement = driver.FindElement(By.Id("droppable"));
+
+            var dragDrop = actions
+                .ClickAndHold(sourceElement)
+                .MoveToElement(targetElement)
+                .Release()
+                .Build();
+
+            dragDrop.Perform();
+
+            Assert.AreEqual("Dropped!", targetElement.Text);
+        }
+    }
+}
---
+++ UserInteractionsdemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UserInteractionsdemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UserInteractionsdemo")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5bc98270-5b56-44d3-a165-ac2843552ac8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
