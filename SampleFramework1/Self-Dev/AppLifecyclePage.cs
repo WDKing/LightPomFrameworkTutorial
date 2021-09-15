90e5d4ed0de2056bec7237d79b9816f8fe7a6773
William King <will.david.king@gmail.com>
2021-09-14 21:01:31 -0500

    20210914 - Section 27, Lesson 327 Submitting first Sprint structure to submit name and have the homepage load with the name in the URL.

---
+++ SampleFramework1/Self-Dev/AppLifecyclePage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SampleFramework1.Self_Dev;
+using System;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace SampleFramework1
+{
+    internal class AppLifecyclePage
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private string URL = "https://ultimateqa.com/sample-application-lifecycle-sprint-1/";
+
+        public AppLifecyclePage(IWebDriver driver, WebDriverWait wait)
+        {
+            this.driver = driver;
+            this.wait = wait;
+        }
+
+        internal void Open()
+        {
+            driver.Navigate().GoToUrl(URL);
+        }
+
+        internal bool IsNameFieldLoaded()
+        {
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@name='firstname']")));
+            return true;
+        }
+
+        internal void FillOutForm(string name)
+        {
+            driver.FindElement(By.XPath("//input[@name='firstname']")).SendKeys(name);
+        }
+
+        internal UltimateQAPage ClickSubmit()
+        {
+            driver.FindElement(By.Id("submitForm")).Click();
+            return new UltimateQAPage(driver, wait);
+        }
+    }
+}
