5ea020cfcbcbed5d4bfcd8abe7e2a5e8328063ab
William King <William.King@centralsquare.com>
2021-09-21 17:46:18 -0500

    20210921 - Quiz for Lesson 375, First Part

--- SampleApp2/Tests/BaseTest.cs
+++ SampleApp2/Tests/BaseTest.cs
@@ -1,18 +1,25 @@
 ﻿using AutomationResources;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace SampleApp2
 {
     public class BaseTest
     {
         public IWebDriver Driver { get; private set; }
+        public WebDriverWait Wait { get; private set; }
+        public Actions Actions { get; private set; }
 
         [TestInitialize]
         public void SetupForEverySingleTestMethod()
         {
             var factory = new WebDriverFactory();
             Driver = factory.Create(BrowserType.Chrome);
+            Actions = new Actions(Driver);
+            Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(6));
         }
 
         [TestCleanup]
